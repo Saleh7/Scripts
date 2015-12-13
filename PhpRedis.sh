a682092e282334c77ad71732e2f2334d599a503c
Saleh Bin Homoud <iPain7@gmail.com>
2015-12-14 02:55:29 +0300

    Update PhpRedis.sh

--- PhpRedis.sh
+++ PhpRedis.sh
@@ -6,13 +6,13 @@
 echo "+-------------------------------------------------+"
 echo "|  Download https://github.com/phpredis/phpredis  |"
 echo "+-------------------------------------------------+"
-git https://github.com/phpredis/phpredis.git
+git clone https://github.com/phpredis/phpredis.git
 
 sudo apt-get install php5-dev -y
 
 cd phpredis && phpize && ./configure && make && make install
 
-sudo sed -i ';   extension=modulename.extension/extension = redis.so/' /etc/php5/fpm/php.ini
+echo "extension=redis.so" >> /etc/php5/fpm/php.ini
 
 sudo service php5-fpm restart
 sudo service nginx restart
